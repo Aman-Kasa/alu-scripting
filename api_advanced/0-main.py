073aa69bd823620aa70b458b2062456c931f1a62
aman <a.kasa@alustudent.com>
2024-10-25 00:13:08 -0700

    module doc added

--- api_advanced/0-main.py
+++ api_advanced/0-main.py
@@ -3,15 +3,14 @@
 0-main
 """
 import sys
-import importlib.util
-
-# Load the module from file
-spec = importlib.util.spec_from_file_location("subs", "0-subs.py")
-subs = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(subs)
+import importlib
 
 if __name__ == '__main__':
+    # Dynamically import the module
+    subs_module = importlib.import_module('0-subs')
+    number_of_subscribers = subs_module.number_of_subscribers
+
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(subs.number_of_subscribers(sys.argv[1])))
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
